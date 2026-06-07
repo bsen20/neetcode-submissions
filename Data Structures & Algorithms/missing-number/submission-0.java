e8afe5ef0986d1c593f3eef6c7806839ca6013b3
bsen20 <75090906+bsen20@users.noreply.github.com>
2026-06-07 17:59:49 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int a:nums)sum+=a;
+        int ans = n*(n+1)/2 - sum;
+
+        return ans;
+    }
+}
