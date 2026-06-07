6c3d794897a3f4e46e425c4ea449040c96fa7648
bsen20 <75090906+bsen20@users.noreply.github.com>
2026-06-07 18:21:10 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0 , ten = 0;
+        for(int b :bills){
+            if(b == 5)
+                five++;
+            else if(b == 10){
+                if(five == 0)
+                    return false;
+                five--;
+                ten++;
+            }else{
+                if(ten>=1 && five>=1){
+                    ten--;
+                    five--;
+                }else if(five>=3){
+                    five-=3;
+                }else
+                    return false;
+            }
+                
+        }
+        return true;
+    }
+}
